88aa4445834a52226527071b47fd828dd0a7b3f4
Exequiel Yuri <leiuqxre@gmail.com>
2022-03-18 11:25:39 +0800

    Add function to determine winner

--- script.js
+++ script.js
@@ -22,10 +22,46 @@ function computerPlay() { // computer's move
 
     switch (randomNumber) { // assign move
         case 0:
-            return 'Rock';
+            return 'rock';
         case 1:
-            return 'Paper';
+            return 'paper';
         case 2:
-            return 'Scissors';
+            return 'scissors';
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    let playerLowerCase = playerSelection.toLowerCase();
+
+    switch (playerLowerCase) {
+        case 'rock':
+            switch (computerSelection) {
+                case 'rock':
+                    return "It's a tie!";
+                case 'paper':
+                    return "You Lose! Paper beats Rock.";
+                case 'scissors':
+                    return "You Win! Rock beats Scissors.";
+            }
+
+        case 'paper':
+            switch (computerSelection) {
+                case 'rock':
+                    return "You Win! Paper beats Rock."
+                case 'paper':
+                    return "It's a tie!";
+                case 'scissors':
+                    return "You Lose! Scissors beats Paper.";
+            }
+
+        case 'scissors':
+            switch (computerSelection) {
+                case 'rock':
+                    return "You Lose! Rock beats Scissors.";
+                case 'paper':
+                    return "You Win! Scissors beats Paper.";
+                case 'scissors':
+                    return "It's a tie!";
+            }
     }
 }
